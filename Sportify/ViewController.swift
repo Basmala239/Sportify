76a56cc6e49287fcca7287dfbf07303b71976581
Basmala239 <abuziedbasmala@gmail.com>
2026-05-19 16:29:45 +0300

    Add alamofire dependancen

--- Sportify/ViewController.swift
+++ Sportify/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import Alamofire // 1. Import the framework
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 2. Trigger the network test when the view loads
+        runAlamofireTest()
     }
 
-
+    func runAlamofireTest() {
+        print("--- Starting Alamofire Test ---")
+        
+        // We use a public test URL that returns a simple JSON response
+        let testURL = "https://httpbin.org/get"
+        
+        AF.request(testURL).responseJSON { response in
+            print("\n--- Network Response Received ---")
+            
+            switch response.result {
+            case .success(let value):
+                print("Success! Alamofire is working perfectly.")
+                print("Response Data: \(value)")
+                
+            case .failure(let error):
+                print("Network Request Failed.")
+                print("Error Details: \(error.localizedDescription)")
+            }
+            
+            print("---------------------------------\n")
+        }
+    }
 }
 
