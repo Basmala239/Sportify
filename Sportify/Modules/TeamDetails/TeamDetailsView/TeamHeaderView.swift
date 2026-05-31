bcd311db2d0b899103ac7b79d44544acfa03a432
Esraa Ehba <esraaehab530@gmail.com>
2026-05-30 18:20:36 -0700

    finished the team header card and attached it to the team details screen

---
+++ Sportify/Modules/TeamDetails/TeamDetailsView/TeamHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  TeamHeaderView.swift
+//  Sportify
+//
+//  Created by Esraa Ehab on 30/05/2026.
+//
+
+import UIKit
+
+class TeamHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
