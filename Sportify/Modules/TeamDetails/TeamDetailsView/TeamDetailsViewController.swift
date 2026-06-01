07e8399cdd934987bc1f8ade83d4a2572744bc4f
Esraa Ehba <esraaehab530@gmail.com>
2026-06-01 13:25:03 -0700

    finished the team fixtures card

--- Sportify/Modules/TeamDetails/TeamDetailsView/TeamDetailsViewController.swift
+++ Sportify/Modules/TeamDetails/TeamDetailsView/TeamDetailsViewController.swift
@@ -11,7 +11,7 @@ class TeamDetailsViewController: UIViewController {
 
     @IBOutlet weak var teamTableView: UITableView!
     
-    let playerSections = ["Goalkeepers", "Defenders", "Midfielders", "Forwards"]
+    let tableSections = ["Goalkeepers", "Defenders", "Midfielders", "Forwards", "Fixtures"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,14 +29,19 @@ class TeamDetailsViewController: UIViewController {
         let nib =  UINib(nibName: "PlayersRowTableViewCell", bundle: nil)
         teamTableView.register(nib, forCellReuseIdentifier: "PlayersRowTableViewCell")
         
+        let fixtureNib = UINib(nibName: "FixtureTableViewCell", bundle: nil)
+        teamTableView.register(fixtureNib, forCellReuseIdentifier: "FixtureTableViewCell")
+        
         teamTableView.dataSource = self
         teamTableView.delegate = self
         
-        teamTableView.rowHeight = 240
+        //teamTableView.rowHeight = 240
         teamTableView.separatorStyle = .none
         
         if #available(iOS 15.0, *) {
            teamTableView.sectionHeaderTopPadding = 40
+            
+            
         }
     }
 }
@@ -45,18 +50,28 @@ class TeamDetailsViewController: UIViewController {
 extension TeamDetailsViewController : UITableViewDataSource , UITableViewDelegate{
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        playerSections.count
+        tableSections.count
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == tableSections.count - 1 {
+                    return 3 
+                }
         return 1
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if indexPath.section == tableSections.count - 1 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "FixtureTableViewCell", for: indexPath) as! FixtureTableViewCell
+            return cell
+        }
+        else{
         let cell = tableView.dequeueReusableCell(withIdentifier: "PlayersRowTableViewCell", for: indexPath) as! PlayersRowTableViewCell
         cell.selectionStyle = .none
         return cell
+       }
     }
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return playerSections[section]
+        return tableSections[section]
     }
     func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
         if let header = view as? UITableViewHeaderFooterView{
@@ -68,4 +83,11 @@ extension TeamDetailsViewController : UITableViewDataSource , UITableViewDelegat
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 50
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            if indexPath.section == tableSections.count - 1 {
+                return 130 // ارتفاع كارت المباراة (ممكن تكبريه أو تصغريه حسب الديزاين بتاعك)
+            }
+            return 260 // الارتفاع اللي كنا ظابطينه لكروت اللعيبة
+        }
 }
